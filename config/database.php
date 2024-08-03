119975a76708c78a0def6858eb4228c957e2b5a4
isaka-james <mr.isakajames@gmail.com>
2024-08-03 19:32:54 +0300

    db: use export commands for storing the sensitive datas

--- config/database.php
+++ config/database.php
@@ -1,9 +1,36 @@
 <?php
 
-$DATABASE_NAME="maindb";
-$SERVERNAME="localhost";
-$DB_USERNAME="root";
-$DB_PASSWORD="";
+/*
+* SET YOUR DATABASE  
+*    CONFIGURATION
+*       HERE!
+*
+*   nb: now only,
+*   mysql is supported!!
+*/ 
+
+$SERVERNAME=getenv('SERVERNAME') ?: 'localhost';
+$DATABASE_NAME=getenv('DB_NAME') ?: 'dbname';
+$DB_USERNAME=getenv('DB_USERNAME') ?: 'root';
+$DB_PASSWORD=getenv('DB_PASSWORD') ?: '';
+
+/*
+ * DATABASE CONFIGURATION
+ *  Type this commands to save the,
+ *   - servername, database name, database username, and password
+ *   
+ *  export DB_USERNAME='your_database_username'
+ *  export DB_PASSWORD='your_database_password'
+ *  export DB_NAME='your_database_name'
+ *  export DB_SERVERNAME='your server name'
+ */
+
+
+
+
+
+
+
 
 
 $db_config = [
