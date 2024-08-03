47f264d41aea1d003ffaa855c1a705f5e06c921f
isaka-james <mr.isakajames@gmail.com>
2024-08-03 18:57:51 +0300

    routes: define your routes here

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Controllers\TestController;
+
+
+// Define routes
+return [
+
+    // Delete this line and implement your logic
+    '/' => [TestController::class, 'testAstra'],
+
+];
